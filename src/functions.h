0c61422c79753537c81c8eddd6884a969eae8990
lukasjanusauskas <lukasjanusauskaslj@gmail.com>
2024-12-18 09:46:57 +0200

    Užbaigtos išvedimo funkcijos

--- src/functions.h
+++ src/functions.h
@@ -7,12 +7,12 @@
 #include <vector>
 
 std::map<std::string, int> get_word_count(std::string file_path);
-void output_word_counts(std::string output_path);
+void output_word_counts(std::string file_path, std::string output_path);
 
 std::multimap<std::string, int> get_cross_ref(std::string file_path);
-void output_word_cross_ref(std::string output_path);
+void output_word_cross_ref(std::string file_path, std::string output_path);
 
 std::set<std::string> get_url_adresses(std::string file_path);
-void output_url_addresses(std::string output_path);
+void output_url_addresses(std::string file_path, std::string output_path);
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -22,9 +22,11 @@ int main() {
   //   std::cout << it->first << " " << it->second << std::endl;
   // }
 
-  auto set_urls = get_url_adresses("data/url-test.txt");
-  for(auto s: set_urls)
-    std::cout << s << std::endl;
+  // auto set_urls = get_url_adresses("data/url-test.txt");
+  // for(auto s: set_urls)
+  //   std::cout << s << std::endl;
+
+  output_word_cross_ref("data/wiki-test.txt", "output/cross-ref.txt");
 
   return 0;
 }
@@ -74,7 +76,6 @@ std::multimap<std::string, int> get_cross_ref(std::string file_path){
     std::stringstream stream;
     std::map<std::string, int> tmp_map;
 
-    std::cout << counter << "\n\n";
     std::getline(fd, line);
 
     stream << line;
@@ -84,6 +85,9 @@ std::multimap<std::string, int> get_cross_ref(std::string file_path){
       for(size_t i = 0; i<tmp_string.length(); ++i)
         if(ispunct(tmp_string[i]))
           tmp_string.erase(i--, 1);
+
+      // Jei skaicius: neiskaiciuojam
+      if(!isalpha(tmp_string[0])) continue;
 
       auto search = tmp_map.find(tmp_string);
       if(search == tmp_map.end()) {
@@ -129,5 +133,46 @@ std::set<std::string> get_url_adresses(std::string file_path) {
       set_urls.insert(tmp_string);
   }
 
+  fd.close();
   return set_urls;
+}
+
+void output_word_counts(std::string file_path, std::string output_path){
+  auto word_counts = get_word_count(file_path);
+
+  std::ofstream fr(output_path);
+  for(auto &[key, value]: word_counts)
+    fr << key << ": " << value << std::endl;
+
+  fr.close();
+}
+
+void output_word_cross_ref(std::string file_path, std::string output_path){
+  auto cross_ref = get_cross_ref(file_path);
+
+  std::ofstream fr(output_path);
+  std::string last = "";
+
+  for(auto it = cross_ref.begin(); it != cross_ref.end(); it++) {
+    if(it->first.compare(last) == 0) {
+      fr << "\t" << it->second;
+    }
+
+    else {
+      fr << "\n" << it->first << " " << it->second;
+      last = it->first;
+    }
+  }
+
+  fr.close();
+}
+
+void output_url_addresses(std::string file_path, std::string output_path){
+  auto set_urls = get_url_adresses(file_path);
+
+  std::ofstream fr(output_path);
+  for(auto s: set_urls)
+    fr << s << std::endl;
+
+  fr.close();
 }
